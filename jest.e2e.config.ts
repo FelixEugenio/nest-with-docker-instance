61be8e118956883eebb1cce9995a7b7b0ff9216b
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-08-01 00:53:13 +0100

    chore: creating database for test e2e

---
+++ jest.e2e.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export default {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    testRegex: '.*\\.e2e-spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+  }
